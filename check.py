69c6fe046889d1255a31ec1f8ff529e5d5ad2f8e
Sergey-Volchkov <seriyvlastelin@gmail.com>
2018-06-01 10:02:31 +0000

    Update check

--- check.py
+++ check.py
@@ -1,16 +1,3 @@
 
 import subprocess
-import psutil
-import time
-name =[]
-
-while True:
-    for proc in psutil.process_iter():
-        name.append(proc.name())
-    try:
-        name.index("python3.5")
-        type(i)
-        time.sleep(300)
-    except:
-        subprocess.Popen("sh scr.sh", shell=True)
-        i=0
+subprocess.Popen("sh scr.sh", shell=True)
