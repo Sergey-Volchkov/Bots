d4bf2d592f5b19294ea05a60e54bf872029b2f6b
Sergey-Volchkov <seriyvlastelin@gmail.com>
2018-06-05 16:45:35 +0300

    Update itisclass.py

--- itisclass.py
+++ itisclass.py
@@ -2,8 +2,8 @@ import random
 def get_photos(vk_session, id_group,vk):
     try:
         attachment = ''
+        max_num = vk.photos.get(owner_id=id_group, album_id='wall', count=0)['count']
         for i in range(5):
-            max_num = vk.photos.get(owner_id=id_group, album_id='wall', count=0)['count']
             num = random.randint(1, max_num)
             pictures = vk.photos.get(owner_id=str(id_group), album_id='wall', count=1, offset=num)['items'][0]['id']
             if len(attachment)<4:
