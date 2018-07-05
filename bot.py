995c9ad693d0862f4d6807ee406d03e6133baee8
Sergey-Volchkov <seriyvlastelin@gmail.com>
2018-07-05 13:08:22 +0300

    1.4
    Обновление до 1.4.
    Long pull

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import sys
 
 sys.path.insert(0, '../')
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 import time
 import vk_api
@@ -10,38 +11,33 @@ from multiprocessing import Process
 import itisclass
 import data
 import follow
-
 login, password = data.data()
 global vk_session
 vk_session = vk_api.VkApi(login, password)
 vk_session.auth()
 global vk
 global id_groups
-
 vk = vk_session.get_api()
 del login, password
 
-
 def choice_group_and_send(mas, response, item, vk_ses, id_group, id):
+
     mas.index(response)
-#     print('До получения пикч ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
     attachment = itisclass.get_photos(vk_ses, id_group, vk)
-    if item.get('chat_id') == None:
-#         print('До отправки сообщения ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
-        write_msg(item[id], 'Держи!', attachment, id)
-#         print('После отправки сообщения ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
-        if id_group == -121355400 and item[id] != 111312042:
+    if id == 'user_id':
+        write_msg(item, 'Держи!', attachment, id)
+        if id_group == -121355400 and item != 111312042:
             write_msg(111312042, 'Кто-то попросил у меня пёселей, но я и тебе пришлю!', attachment, id)
     else:
-        write_msg(item[id], 'Держите!', attachment, id)
+        write_msg(item, 'Держите!', attachment, id)
 
 
 def write_msg(user_id, s, attachment, id):
     vk_session.method('messages.send', {id: user_id, 'message': s, 'attachment': attachment})
 
 
-def commands(item):
-    vk_session.method('messages.send', {'user_id': item['user_id'],
+def commands(user_id):
+    vk_session.method('messages.send', {'user_id': user_id,
                                         'message': "Команды для бота: \n\n Для получения лолей напишите что-то из 'лоли', 'лольки', 'loli', 'лолька', 'лоля', 'лоликон'.\n \n"
                                                    " Для получения котиков напишите что-то из 'котик', 'кошка', 'кот', 'котенок', 'котяра', 'cat', 'котика', 'котики', 'коты', 'cats', 'пушистый педрила','киса','котейка','котейки','кисы', 'пушистые педрилы', 'пушистые пидрилы', 'пушистая педрила', 'пушастый пидрила', 'шаверма', 'шаурма', 'котя'.\n \n"
                                                    "Для получения пёселей напишите что-то из 'пёсель', 'собака', 'пёс', 'doge', 'песель', 'псина', 'пёсели', 'песели', 'псины', 'пёсики', 'песики', 'хлеп', 'хлеб', 'булочка', 'булочки', 'собакен', 'собакены', 'dog', 'dogs'.\n \n"
@@ -59,44 +55,39 @@ def check_message(all_commands, response, item, vk_ses, id, fw):
     for key in all_commands:
         try:
             choice_group_and_send(all_commands[key][1], response, item, vk_ses, all_commands[key][0], id)
-
             break
         except:
             pass
     if id == 'user_id':
         try:
-            vk_session.method('messages.send', {'user_id': item['user_id'],
-                                                'message': fw.main_f(response, item['user_id'], all_commands)})
+            vk_session.method('messages.send', {'user_id': item,
+                                                'message': fw.main_f(response, item, all_commands)})
         except:
             pass
 
 
-def send_picture(values, all_commands):
+def send_picture(all_commands):
     vk_ses = vk_session
     fw = follow.subscription()
+    longpoll = VkLongPoll(vk_session)
     while True:
         try:
-            response = vk_session.method('messages.get', values)
-            if response['items']:
-                print('Сообщение пришло в ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
-                response['items'].reverse()
-            for item in response['items']:
-                response = item['body'].lower()
-                if item.get('chat_id') == None:
-                    print(item['user_id'])
-                    if response == 'команды':
-                        commands(item)
-                    elif response == 'на что я подписан?' or response == 'на что я подписан':
-                        vk_session.method('messages.send', {'user_id': item['user_id'],
-                                                            'message': fw.list_of_subscribers(item['user_id'],
-                                                                                              all_commands)})
-                    check_message(all_commands, response, item, vk_ses, 'user_id', fw)
-                else:
-                    print(item['chat_id'])
-                    check_message(all_commands, response, item, vk_ses, 'chat_id', fw)
-                values['last_message_id'] = item['id']
-
-            time.sleep(1)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    print('Сообщение пришло в ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
+                    response = event.text.lower()
+                    if event.from_user:
+                        print(event.user_id)
+                        if response == 'команды':
+                            commands(event.user_id)
+                        elif response == 'на что я подписан?' or response == 'на что я подписан':
+                            vk_session.method('messages.send', {'user_id': event.user_id,
+                                                                'message': fw.list_of_subscribers(event.user_id,
+                                                                                                  all_commands)})
+                        check_message(all_commands, response, event.user_id, vk_ses, 'user_id', fw)
+                    else:
+                        print(event.chat_id)
+                        check_message(all_commands, response, event.group_id, vk_ses, 'chat_id', fw)
         except:
             pass
 
@@ -119,7 +110,6 @@ def dispatch_module(vk_session, id_groups, vk, d):
 def dispatch(id_groups):
     # try:
     while True:
-        time.sleep(10800)
         f = open('../subscribers.txt', 'r')
         d = eval(f.read())
         f.close()
@@ -127,6 +117,7 @@ def dispatch(id_groups):
         if 22 > int(datetime.strftime(datetime.now(), "%H")) + 3 >= 8:
             dispatch_module(vk_session, id_groups, vk, d)
         print('В сон на 60 минут')
+        time.sleep(10800)
 
     # except FileNotFoundError:
     #     print('Файла нет')
@@ -138,7 +129,6 @@ def dispatch(id_groups):
 
 
 def main():
-    values = {'out': 0, 'count': 20, 'time_offset': 120}
     all_commands = {'cat': [-130670107,
                             ['котик', 'кошка', 'кот', 'котенок', 'котяра', 'cat', 'котика', 'котики', 'коты', 'cats',
                              'пушистый педрила', 'киса', 'котейка', 'котейки', 'кисы', 'пушистые педрилы',
@@ -152,7 +142,7 @@ def main():
                     'fox': [-123223613, ['лиса', 'лисы', 'лисичка', 'лисички', 'fox', 'фокс', 'фоксы', 'foxes'], 'лис'],
                     'owl': [-113854206, ['совы', 'совушки', 'сов', 'owl', 'owls','сова'], 'сов']}
 
-    p1 = Process(target=send_picture, args=(values, all_commands,))
+    p1 = Process(target=send_picture, args=(all_commands,))
     p2 = Process(target=dispatch, args=(all_commands,))
     p1.start()
     p2.start()
